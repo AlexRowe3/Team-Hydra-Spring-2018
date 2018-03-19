fdbdf9ebbc44660fd7be2957781eb2ee76b60914
AlexRowe3 <arowe3@ggc.edu>
2018-03-19 16:33:45 -0400

    Alex first Commit

--- src/example1.java
+++ src/example1.java
@@ -4,6 +4,9 @@ public class example1 {
 	public static void main (String args[])
 	{
 		System.out.println("hello, this line as been added by lade");
-		
+
+
+		System.out.println("hello, this line as been added by alex");
+
 	}
 }
