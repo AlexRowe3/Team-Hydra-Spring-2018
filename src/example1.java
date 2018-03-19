e9ad2958f5a1b765f27032c68600b425bb50016d
Tom Biedenbach <tbiedenbach@ggc.edu>
2018-03-19 16:41:35 -0400

    Tom's first commit

--- src/example1.java
+++ src/example1.java
@@ -4,9 +4,8 @@ public class example1 {
 	public static void main (String args[])
 	{
 		System.out.println("hello, this line as been added by lade");
-
-
 		System.out.println("hello, this line as been added by alex");
+		System.out.println("hello, this line has been added by tom");
 
 	}
 }
