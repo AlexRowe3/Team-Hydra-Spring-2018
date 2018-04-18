5a870b8e00a6cfe2ec5680b9c5800bd99673034c
wbullockGGC <wbullock@ggc.edu>
2018-04-18 17:02:45 -0400

    Added the blueprint class for storage of what they make/need

---
+++ src/model/Blueprint.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Blueprint extends GenericItem {
+	
+	ArrayList<GenericItem> requiredItems;
+	GenericItem output;
+	public Blueprint(String itemName, String itemShortName, String description, ArrayList<GenericItem> requiredItems, GenericItem output) {
+		super(itemName, itemShortName, description);
+		this.requiredItems = requiredItems;
+		this.output = output;
+	}
+	
+	public ArrayList<GenericItem> getRequiredItems() {
+		return requiredItems;
+	}
+	
+	public GenericItem getOutput() {
+		return output;
+	}
+	
+}
